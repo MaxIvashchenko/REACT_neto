d30a5361a152fb6d1925f86df47b00563447ba3c
Max Ivashchenko <Max@MacBook-Pro-Max.local>
2020-01-10 17:52:55 +0200

    add fold lesson_2

---
+++ lesson_2/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+import func from './components/func';
+
+
+
+function App() {
+  
+  return (
+    <>
+    <func />
+    <button>rre</button>
+    </>
+  )
+
+}
+
+export default App;
